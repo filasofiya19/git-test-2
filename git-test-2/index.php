30856edd1b0e1d436a7bf0abd93a4e3e3a7b3725
filasofiya19 <filasofiyati19@gmail.com>
2024-04-17 20:28:28 +0700

    Menambahkan paragraf baru

--- git-test-2/index.php
+++ git-test-2/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello Fila!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque enim tenetur sint veritatis, impedit dicta facere consequuntur eaque non porro maiores ipsam laborum beatae! Velit animi enim aut officiis quidem!</p>
 </body>
 </html>
